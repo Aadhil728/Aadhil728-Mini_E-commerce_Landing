0fd0a2c33416623253008799ac2c1a4a909edede
Aadhil728 <mohamedaadhil0728@gmail.com>
2024-09-11 23:41:02 +0530

    add new server file

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+//middleware
+app.use(
+  cors({
+    origin: ["https://aadhil728-mini-e-commerce-landing.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.send("Welcome to the Mini E-commerce API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
